447781dff931eb1d7e350148d0c1c7792e1efd95
ekaterinahubkina <ekaterinahubkina@yandex.by>
2022-02-04 00:56:44 +0300

    implemented card like and delete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,13 +4,28 @@ import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
 import PopupWithForm from '../PopupWithForm/PopupWithForm';
 import ImagePopup from '../ImagePopup/ImagePopup';
+import api from '../../utils/api';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 
 function App() {
 
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
-  const [selectedCard, setSelectedCard] = React.useState({})
+  const [selectedCard, setSelectedCard] = React.useState({});
+
+  //Создайте стейт currentUser в корневом компоненте
+  const [currentUser, setCurrentUser] = React.useState({});
+
+  React.useEffect(() => {
+    api.getUserData()
+      .then(res => {
+        console.log(res);
+        setCurrentUser(res);
+      })
+  }, [])
+
+
 
   function handleEditAvatarClick () {
     setIsEditAvatarPopupOpen(true);
@@ -37,6 +52,8 @@ function App() {
 
 
   return (
+    //Создайте объект контекста и используйте провайдер
+    <CurrentUserContext.Provider value={currentUser}>
     <div className="page">
       <Header />
       <Main onEditProfile={handleEditProfileClick}
@@ -64,6 +81,7 @@ function App() {
       </PopupWithForm>
       <ImagePopup card={selectedCard} onClose={closeAllPopups}/>
     </div>
+    </CurrentUserContext.Provider>
     
 
   );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,25 +1,35 @@
 import api from '../../utils/api';
 import React from 'react';
 import Card from '../Card/Card';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 
 function Main (props) {
 
-    const [userAvatar, setUserAvatar] = React.useState('');
-    const [userName, setUserName] = React.useState('');
-    const [userDescription, setUserDescription] = React.useState('');
+    const currentUser = React.useContext(CurrentUserContext);
 
-    React.useEffect(() => {
-        api.getUserData()
-            .then(res => {
+    function handleCardLike(card) {
+        const isLiked = card.likes.some(like => like._id === currentUser._id);
+
+        (!isLiked) ?
+        api.putLike(card)
+            .then((newCard) => {
+                setCards((state) => state.map((c) => c._id === card._id ? newCard : c))
+        }) :
+        api.deleteLike(card)
+            .then((newCard) => {
+                setCards((state) => state.map((c) => c._id === card._id ? newCard : c))
+        })
+    }
+
+    function handleCardDelete(card) {
+        const isOwn = card.owner._id === currentUser._id;
+
+        isOwn && api.deleteMyCard(card)
+            .then((res) => {
                 console.log(res);
-                setUserAvatar(res.avatar);
-                setUserName(res.name);
-                setUserDescription(res.about);        
+                setCards((state) => state.filter((c) => c._id !== card._id))
             })
-            .catch(err => {
-                console.log(err)
-            })
-    }, []);
+    }
 
     const [cards, setCards] = React.useState([]);
 
@@ -34,19 +44,17 @@ function Main (props) {
             })
     }, [])
 
-
-
     return (
         <main className="content">
             <section className="profile">
                 <div className="profile__main-info">
                     <div className="profile__avatar-container">
-                        <img className="profile__avatar" src={userAvatar} alt="Аватар" onClick={props.onEditAvatar}/>
+                        <img className="profile__avatar" src={currentUser.avatar} alt="Аватар" onClick={props.onEditAvatar}/>
                         <div className="profile__avatar-hover"></div>
                     </div>
                     <div className="profile__info">
-                        <h1 className="profile__name">{userName}</h1>
-                        <p className="profile__occupation">{userDescription}</p>
+                        <h1 className="profile__name">{currentUser.name}</h1>
+                        <p className="profile__occupation">{currentUser.about}</p>
                         <button className="profile__edit-button" type="button" aria-label="Редактировать профиль" onClick={props.onEditAvatar}></button>
                     </div>
                 </div>
@@ -55,7 +63,7 @@ function Main (props) {
             <section className="cards" aria-label="Карточки мест">
                 {cards.map((item) => {
                     return (
-                        <Card card={item} {...item} key={item._id} onCardClick={props.onCardClick}/>                
+                        <Card card={item} {...item} key={item._id} onCardClick={props.onCardClick} onCardLike={handleCardLike} onCardDelete={handleCardDelete}/>                
                     )
                 }
                 )}
